4510ab025cdf1b27485309f235f18af6a91a2943
sean-jordan <104404401+sean-jordan@users.noreply.github.com>
2022-12-12 08:11:14 -0500

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -1,183 +1,25 @@
-var docHeight
+// Declare a variable for the image
+let img;
+
+function preload() {
+  // Load the image in the preload() function
+  img = loadImage("https://i.imgur.com/GkbtYrG.png");
+}
 
 function setup() {
-  createCanvas(50, 6000);
+  // Create a canvas to draw the image on
+  createCanvas(3000, 3000);
+
+  // Set the image alignment mode to center
+  imageMode(CENTER);
 }
 
 function draw() {
-  imageSmoothingEnabled = true;
-  background(255,255,255);
-  strokeWeight(5);
-  stroke(5);
-  line(20, 20, 20, window.innerHeight - 20);
+  // Clear the canvas
+  clear();
 
-  docHeight = Math.max(
-    document.body.scrollHeight, document.documentElement.scrollHeight,
-    document.body.offsetHeight, document.documentElement.offsetHeight,
-    document.body.clientHeight, document.documentElement.clientHeight
-  );
-
-  // This is where you add the IDs of the elements
-  // you wish to connect
-
-  
-  var top = (window.pageYOffset || document.documentElement.scrollTop)  -     (document.documentElement.clientTop || 0);
-  
-  var bottom = top+window.innerHeight;
-  
-  
-  
-  let mapTop = map(top, 0, docHeight, 20, window.innerHeight-20)
-  let mapBottom = map(bottom, 0, docHeight, 20, window.innerHeight-20)
-  
-  // ellipse(20,mapTop,20,20);
-  // ellipse(20,mapBottom,20,20);
-
-  fill(100);
-  rect(18.5,mapTop,3,mapBottom-mapTop);
-  
-  highlight("Fact 1", top,bottom);
-  highlight("Graphs", top,bottom);
-  highlight("Fact 3", top,bottom);
-  highlight("Fact 2", top,bottom);
-    
-  createLink("Fact 1");
-  createLink("Graphs");
-  createLink("Fact 3");
-  createLink("Fact 2");
-  
-
-  
+  // Use the image() function to draw the image at the cursor position,
+  // with the desired width and height
+  image(img, mouseX, mouseY, 55, 35);
 }
 
-function getPos(el) {
-  // yay readability
-  for (var lx = 0, ly = 0; el != null; lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
-  return {
-    x: lx,
-    y: ly,
-  }
-
-}
-
-function createLink(divElement) {
-
-  let segPos = getPos(document.getElementById(divElement));
-  let scrollLocSeg = ((segPos.y / docHeight) * window.innerHeight - 40 + 20);
-  
-  fill(0,0,0);
-    noStroke();
-  rectMode(CENTER);
-  rect(20,scrollLocSeg+20,6,textWidth(divElement)+15);
-  rectMode(CORNER);
-  
-    if (dist(mouseX,mouseY,20,scrollLocSeg+20)<40){
-    console.log("mouse is over");
-  fill(0,0,0);
-    noStroke();
-  rectMode(CENTER);
-  rect(20,scrollLocSeg+20,6,textWidth(divElement)+15);
-  rectMode(CORNER);
-      
-  // cursor = "pointer";
-  }
-  
-  push();
-  translate(23, scrollLocSeg + 20);
-  
-  rotate(-PI / 2);
-  noStroke();
-  fill(255);
-  textAlign(CENTER);
-  text(divElement, 0, 0);
-  pop();
-  stroke(255);
-  line(20+10, scrollLocSeg + 20, segPos.x, segPos.y - window.scrollY);
-  
-
-  
-}
-
-
-function highlight(divElement, t,b){
-
-  let segPos = getPos(document.getElementById(divElement));
-  let scrollLocSeg = ((segPos.y / docHeight) * window.innerHeight - 40 + 20);
-  
-  var divRect = document.getElementById(divElement).getBoundingClientRect();
-  var height = divRect.height;
-  
-  // console.log(divRect.height);
-  
-  if (segPos.y<=b && segPos.y+divRect.height>=t){
-    rectMode(CENTER);
-    rect(20,scrollLocSeg+20,10,textWidth(divElement)+25);
-    rectMode(CORNER);
-    
-  function mouseClicked(){
-    
-    document.documentElement.scrollTop = 0;
-    // console.log("is in view");
-    
-  }
-  }
-  
-}
-
-//////
-
-class internalLink {
-  
-  constructor() {
-    this.x;
-    this.y;
-    this.name;
-  }
-  
-  highlight2(divElement,t,b){
-    
-     let segPos = getPos(document.getElementById(divElement));
-  let scrollLocSeg = ((segPos.y / docHeight) * window.innerHeight - 40 + 20);
-  
-  var divRect = document.getElementById(divElement).getBoundingClientRect();
-  var height = divRect.height;
-  
-  // console.log(divRect.height);
-  
-  if (segPos.y<=b && segPos.y+divRect.height>=t){
-   rectMode(CENTER);
-    rect(20,scrollLocSeg+20,10,textWidth(divElement)+25);
-  rectMode(CORNER);
-    
-  }
-    
-  }
-    
-    
-  createLink2(divElement) {
-
-  let segPos = getPos(document.getElementById(divElement));
-  let scrollLocSeg = ((segPos.y / docHeight) * window.innerHeight - 40 + 20);
-  
-  fill(0,0,255);
-    noStroke();
-  rectMode(CENTER);
-  rect(20,scrollLocSeg+20,6,textWidth(divElement)+15);
-  rectMode(CORNER);
-  
-  push();
-  translate(23, scrollLocSeg + 20);
-  
-  rotate(-PI / 2);
-  noStroke();
-  fill(255);
-  textAlign(CENTER);
-  text(divElement, 0, 0);
-  pop();
-  stroke(255);
-  line(20+10, scrollLocSeg + 20, segPos.x, segPos.y - window.scrollY);
-  
-}
-    
-  
-}
